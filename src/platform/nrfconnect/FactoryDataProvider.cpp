4b856d67e036c2a770c59412288b63b05d049ddb
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2022-07-12 12:27:18 +0200

    Added using CD from config in case settings read failed

--- src/platform/nrfconnect/FactoryDataProvider.cpp
+++ src/platform/nrfconnect/FactoryDataProvider.cpp
@@ -92,18 +92,17 @@ CHIP_ERROR FactoryDataProvider<FlashFactoryData>::GetCertificationDeclaration(Mu
 #if CONFIG_CHIP_CERTIFICATION_DECLARATION_STORAGE
     size_t cdLen = 0;
 
-    ReturnErrorOnFailure(Internal::ZephyrConfig::ReadConfigValueStr(Internal::ZephyrConfig::kConfigKey_CertificationDeclaration,
-                                                                    reinterpret_cast<char *>(outBuffer.data()), outBuffer.size(),
-                                                                    cdLen));
-
-    outBuffer.reduce_size(cdLen);
-
-    return CHIP_NO_ERROR;
-#else
+    if (Internal::ZephyrConfig::ReadConfigValueBin(Internal::ZephyrConfig::kConfigKey_CertificationDeclaration,
+                                                   reinterpret_cast<uint8_t *>(outBuffer.data()), outBuffer.size(),
+                                                   cdLen) == CHIP_NO_ERROR)
+    {
+        outBuffer.reduce_size(cdLen);
+        return CHIP_NO_ERROR;
+    }
+#endif
     constexpr uint8_t kCdForAllExamples[] = CHIP_DEVICE_CONFIG_CERTIFICATION_DECLARATION;
 
     return CopySpanToMutableSpan(ByteSpan{ kCdForAllExamples }, outBuffer);
-#endif // CONFIG_CHIP_CERTIFICATION_DECLARATION_STORAGE
 }
 
 template <class FlashFactoryData>
